76be7f28ae198dbf4b2638d5c5fdf4abe3d0c22b
AranParrish <153287399+AranParrish@users.noreply.github.com>
2025-12-09 14:54:01 +0000

    add data validation tests

--- test/test_utils.py
+++ test/test_utils.py
@@ -10,7 +10,7 @@ def cleansed_df():
     return load_data(Path("data/valid_test_data/"))
 
 
-@pytest.mark.describe("Train / Test Split tests")
+@pytest.mark.describe("Train / Test function tests")
 class TestTrainTestSplit:
 
     @pytest.mark.it("Input is not mutated")
@@ -30,3 +30,76 @@ class TestTrainTestSplit:
         assert len(X_train.columns) == 9
         assert len(X_test.columns) == 9
         assert "price" in y_train.columns and "price" in y_test.columns
+
+    @pytest.mark.it("Returns expected train and test sample sizes")
+    def test_train_test_sizes(self, cleansed_df):
+        X_train, X_test, y_train, y_test = train_test_datasets(
+            cleansed_df, target_col="price", test_size=0.2
+        )
+        expected_train_size = len(cleansed_df) * 0.8
+        expected_test_size = len(cleansed_df) * 0.2
+        assert len(X_train) == expected_train_size
+        assert len(y_train) == expected_train_size
+        assert len(X_test) == expected_test_size
+        assert len(y_train) == expected_train_size
+
+
+@pytest.mark.describe("Train / Test exception handling")
+class TestTrainTestExceptions:
+
+    @pytest.mark.it("Raises TypeError if input is not a dataframe")
+    def test_input_not_a_dataframe(self):
+        invalid_input = []
+        with pytest.raises(TypeError) as excinfo:
+            train_test_datasets(invalid_input, target_col="price")
+        assert "Input dataset must be a pandas dataframe" in str(excinfo.value)
+
+    @pytest.mark.it("Raises ValueError if target col does not exist")
+    def test_target_col_does_not_exist(self, cleansed_df):
+        with pytest.raises(ValueError) as excinfo:
+            train_test_datasets(cleansed_df, target_col="invalid")
+        assert "Target column not in input dataset" in str(excinfo.value)
+
+    @pytest.mark.it("Raises TypeError if dataframe does not contain features")
+    def test_df_without_features(self, cleansed_df):
+        invalid_df = cleansed_df[["price"]]
+        with pytest.raises(TypeError) as excinfo:
+            train_test_datasets(invalid_df, target_col="price")
+        assert "Dataframe does not contain any features" in str(excinfo.value)
+
+    @pytest.mark.it("Raises ValueError if given invalid test size proportion")
+    def test_invalid_test_size_float(self, cleansed_df):
+        with pytest.raises(ValueError) as excinfo:
+            train_test_datasets(cleansed_df, target_col="price", test_size=1.1)
+        assert (
+            "test_size must be a float between 0.0 and 1.0 or an integer number of samples"
+            in str(excinfo.value)
+        )
+
+    @pytest.mark.it("Raises ValueError if given test number of samples")
+    def test_invalid_test_size_int(self, cleansed_df):
+        invalid_sample_size = len(cleansed_df) + 1
+        with pytest.raises(ValueError) as excinfo:
+            train_test_datasets(
+                cleansed_df, target_col="price", test_size=invalid_sample_size
+            )
+        assert (
+            "test_size must be a float between 0.0 and 1.0 or an integer number of samples"
+            in str(excinfo.value)
+        )
+
+    @pytest.mark.it("Raises TypeError if random seed is not a valid type")
+    def test_random_seed_invalid_type(self, cleansed_df):
+        with pytest.raises(TypeError) as excinfo:
+            train_test_datasets(cleansed_df, target_col="price", random_seed="42")
+        assert "random_seed must be an integer" in str(excinfo.value)
+
+    @pytest.mark.it(
+        "Raises ValueError if random seed is outside the valid integer range"
+    )
+    def test_random_seed_outside_integer_limits(self, cleansed_df):
+        with pytest.raises(ValueError) as excinfo:
+            train_test_datasets(cleansed_df, target_col="price", random_seed=-1)
+        assert "random_seed must be an integer in the range [0, 2**32 - 1]" in str(
+            excinfo.value
+        )
