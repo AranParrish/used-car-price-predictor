3bf6d6ca4e37dea97244e499eee63ebf00218142
AranParrish <153287399+AranParrish@users.noreply.github.com>
2025-12-19 16:46:18 +0000

    refactor and add exception handling for split_and_tensorise function

--- src/utils.py
+++ src/utils.py
@@ -40,74 +40,51 @@ def linear_train_test_datasets(
     target_col: str | int | float | tuple | datetime,
     test_size: int | float = 0.2,
     random_seed: RandomState | int = 42,
-) -> tuple:
+) -> list[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
     """
     Function to split given dataset into test and training sets for linear regression ML models
 
     Args:
         df - Numeric pandas dataframe containing full dataset (features and target)
         target_col - String name of the target column (i.e. y values, all remaining columns used as features)
-        test_size - Proportion of data to use as test set, remaining data used for training set.
+        (Optional) test_size - Proportion of data to use as test set, remaining data used for training set.
                     Can be given as a proportion (between 0.0 and 1.0) or absolute integer number of samples.
                     Default value of 0.2.
-        random_seed - Seed value to ensure repeatable split of data (i.e. to ensure same data splits for comparing ML models). Default value of 42.
+        (Optional) random_seed - Seed value to ensure repeatable split of data (i.e. to ensure same data splits for comparing ML models). Default value of 42.
 
     Returns:
-        Four datasets as a tuple - two training sets (of features and target data) and two testing sets (of features and target data)
+        Four datasets as a list - two training sets (of features and target data) and two testing sets (of features and target data)
 
     Raises:
-        TypeError if:
-            - input data is not a pandas dataframe
-            - input data contains non-numeric columns
-            - input data does not contain any features
-            - the random_seed is not an integer
+        TypeError if input data is not a pandas dataframe
         ValueError if:
             - input target column is not present in the dataframe
-            - test_size is not a proportion between 0.0 and 1.0 or a valid number of samples
-            - random_seed is outside the range [0, 2***32 - 1]
+            - input data contains non-numeric columns
+            - input data does not contain any features
     """
     if not isinstance(df, pd.DataFrame):
         raise TypeError("Input dataset must be a pandas dataframe")
 
     if not df.select_dtypes(include=["object", "string", "boolean"]).empty:
-        raise TypeError("Input dataframe must not contain non-numeric columns")
+        raise ValueError("Input dataframe must not contain non-numeric columns")
 
     if len(df.columns) < 2:
-        raise TypeError(
+        raise ValueError(
             "Dataframe must contain at least one feature column and one target column"
         )
 
-    if isinstance(test_size, float) and not 0.0 <= test_size <= 1.0:
-        raise ValueError(
-            "test_size must be a float between 0.0 and 1.0 or an integer number of samples"
-        )
-
-    if isinstance(test_size, int) and not 0 <= test_size <= len(df):
-        raise ValueError(
-            "test_size must be a float between 0.0 and 1.0 or an integer number of samples"
-        )
-
-    if not isinstance(random_seed, (int, RandomState)):
-        raise TypeError("random_seed must be an integer or numpy RandomState instance")
-
-    if isinstance(random_seed, int) and not 0 <= random_seed <= 2**32 - 1:
-        raise ValueError("random_seed must be an integer in the range [0, 2**32 - 1]")
-
-    try:
-        y = df[[target_col]]
-        X = df.drop(columns=target_col, axis=1)
-    except KeyError:
+    if target_col not in df.columns:
         raise ValueError("Target column not in input dataset")
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=test_size, random_state=random_seed
-    )
-    return X_train, X_test, y_train, y_test
+    y = df[target_col]
+    X = df.drop(columns=target_col, axis=1)
+
+    return train_test_split(X, y, test_size=test_size, random_state=random_seed)
 
 
 def embeddings_preprocessing(
     df: pd.DataFrame, target_col: str | int | float | datetime | tuple
-) -> tuple:
+) -> tuple[pd.DataFrame, pd.DataFrame, pd.Series, dict]:
     """
     Function to preprocess a dataframe being used for a PyTorch Neural Network ML model.
 
@@ -119,7 +96,7 @@ def embeddings_preprocessing(
 
     Returns:
         A tuple containing a dataframe of numerical features, a dataframe of categorical features,
-        a dataframe containing target values, and a dictionary of mappings for the categorical features.
+        a series containing target values, and a dictionary of mappings for the categorical features.
 
     Raises:
         TypeError if input data is not a pandas dataframe.
@@ -136,7 +113,7 @@ def embeddings_preprocessing(
     if df.isna().values.any():
         raise ValueError("Input data contains invalid rows")
 
-    y = df[[target_col]].reset_index(drop=True)
+    y = df[target_col].reset_index(drop=True)
     X = df.drop(columns=target_col)
     num_cols = X.select_dtypes(include=["number"]).columns
     cat_cols = X.select_dtypes(include=["object", "string", "boolean"]).columns
@@ -155,38 +132,49 @@ def embeddings_preprocessing(
 def split_and_tensorise(
     X_num: pd.DataFrame,
     X_cat: pd.DataFrame,
-    y: pd.DataFrame,
+    y: pd.Series,
     test_size: int | float = 0.2,
     random_seed: RandomState | int = 42,
-) -> tuple:
+) -> tuple[
+    torch.Tensor,
+    torch.Tensor,
+    torch.Tensor,
+    torch.Tensor,
+    torch.Tensor,
+    torch.Tensor,
+]:
     """
     Function to create training and testing datasets as torch tensors for use in a PyTorch model.
 
     Args:
         X_num - Pandas dataframe of numerical features data.
         X_cat - Pandas dataframe of categorical features data (as an embeddings layer).
-        y - Pandas dataframe of target feature data.
+        y - Pandas series of target data.
+        (Optional) test_size - Proportion of data to use as test set, remaining data used for training set.
+                    Can be given as a proportion (between 0.0 and 1.0) or absolute integer number of samples.
+                    Default value of 0.2.
+        (Optional) random_seed - Seed value to ensure repeatable split of data (i.e. to ensure same data splits for comparing ML models). Default value of 42.
 
     Returns:
         A tuple containing six datasets (training and testing sets for X_num, X_cat, and y).
 
     Raises:
         TypeError if:
-            - any input is not a pandas dataframe
-            - any input contains non-numeric values
-        ValueError if the length of the inputs do not match
+            - either input features are not a pandas dataframe
+            - the target data is not a pandas series
+        ValueError if any input data contains non-numeric values
     """
-    # if not all(isinstance(data, pd.DataFrame) for data in (X_num, X_cat, y)):
-    #     raise TypeError("Input data must all be a pandas dataframe")
+    if not all(isinstance(data, pd.DataFrame) for data in (X_num, X_cat)):
+        raise TypeError("Input features must be pandas dataframe")
 
-    # if (
-    #     not X.select_dtypes(include=["object", "string"]).empty
-    #     or not y.select_dtypes(include=["object", "string"]).empty
-    # ):
-    #     raise TypeError("Inputs must not contain non-numeric values")
+    if not isinstance(y, pd.Series):
+        raise TypeError("Target must be a pandas series")
 
-    # if len(X) != len(y):
-    #     raise ValueError("X and y lengths must match")
+    if not X_num.select_dtypes(exclude=["number"]).empty:
+        raise ValueError("X_num must only contain numeric values")
+
+    if not X_cat.select_dtypes(exclude=["integer"]).empty:
+        raise ValueError("X_cat must only contain integer category codes")
 
     X_num_train, X_num_test, X_cat_train, X_cat_test, y_train, y_test = (
         train_test_split(X_num, X_cat, y, test_size=test_size, random_state=random_seed)
@@ -197,6 +185,6 @@ def split_and_tensorise(
         torch.tensor(X_num_test.values, dtype=torch.float32),
         torch.tensor(X_cat_train.values, dtype=torch.long),
         torch.tensor(X_cat_test.values, dtype=torch.long),
-        torch.tensor(y_train.values, dtype=torch.float32),
-        torch.tensor(y_test.values, dtype=torch.float32),
+        torch.tensor(y_train.to_numpy(), dtype=torch.float32).unsqueeze(1),
+        torch.tensor(y_test.to_numpy(), dtype=torch.float32).unsqueeze(1),
     )
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,5 +1,6 @@
 import pytest, torch
 import pandas as pd
+import numpy as np
 from pathlib import Path
 from src.utils import (
     linear_train_test_datasets,
@@ -84,14 +85,17 @@ class TestLinearTrainTestSplit:
 
     @pytest.mark.it("Returns expected output structure")
     def test_output_structure(self, linear_processed_df):
-        output = linear_train_test_datasets(linear_processed_df, target_col="price")
-        expected_numeric_features = ["year", "mileage", "tax", "mpg", "engineSize"]
-        assert isinstance(output, tuple)
-        assert len(output) == 4
-        X_train, X_test, y_train, y_test = output
-        assert all(feature in X_train.columns for feature in expected_numeric_features)
-        assert all(feature in X_test.columns for feature in expected_numeric_features)
-        assert "price" in y_train.columns and "price" in y_test.columns
+        outputs = linear_train_test_datasets(linear_processed_df, target_col="price")
+        assert isinstance(outputs, list)
+        assert len(outputs) == 4
+        X_train, X_test, y_train, y_test = outputs
+        assert all(
+            isinstance(features_data, pd.DataFrame)
+            for features_data in (X_train, X_test)
+        )
+        assert all(
+            isinstance(target_data, pd.Series) for target_data in (y_train, y_test)
+        )
 
     @pytest.mark.it("Returns expected train and test sample sizes")
     def test_train_test_sizes(self, linear_processed_df):
@@ -116,9 +120,9 @@ class TestLinearTrainTestExceptions:
             linear_train_test_datasets(invalid_input, target_col="price")
         assert "Input dataset must be a pandas dataframe" in str(excinfo.value)
 
-    @pytest.mark.it("Raises TypeError if input dataframe contains non-numeric columns")
+    @pytest.mark.it("Raises ValueError if input dataframe contains non-numeric columns")
     def test_input_non_numeric_cols(self, cleansed_df):
-        with pytest.raises(TypeError) as excinfo:
+        with pytest.raises(ValueError) as excinfo:
             linear_train_test_datasets(cleansed_df, target_col="price")
         assert "Input dataframe must not contain non-numeric columns" in str(
             excinfo.value
@@ -131,58 +135,15 @@ class TestLinearTrainTestExceptions:
         assert "Target column not in input dataset" in str(excinfo.value)
 
     @pytest.mark.it(
-        "Raises TypeError if dataframe does not contain at least one feature and one target"
+        "Raises ValueError if dataframe does not contain at least one feature and one target"
     )
     def test_df_without_features(self, linear_processed_df):
         invalid_df = linear_processed_df[["price"]]
-        with pytest.raises(TypeError) as excinfo:
+        with pytest.raises(ValueError) as excinfo:
             linear_train_test_datasets(invalid_df, target_col="price")
         assert (
             "Dataframe must contain at least one feature column and one target column"
             in str(excinfo.value)
-        )
-
-    @pytest.mark.it("Raises ValueError if given invalid test size proportion")
-    def test_invalid_test_size_float(self, linear_processed_df):
-        with pytest.raises(ValueError) as excinfo:
-            linear_train_test_datasets(
-                linear_processed_df, target_col="price", test_size=1.1
-            )
-        assert (
-            "test_size must be a float between 0.0 and 1.0 or an integer number of samples"
-            in str(excinfo.value)
-        )
-
-    @pytest.mark.it("Raises ValueError if given test number of samples")
-    def test_invalid_test_size_int(self, linear_processed_df):
-        invalid_sample_size = len(linear_processed_df) + 1
-        with pytest.raises(ValueError) as excinfo:
-            linear_train_test_datasets(
-                linear_processed_df, target_col="price", test_size=invalid_sample_size
-            )
-        assert (
-            "test_size must be a float between 0.0 and 1.0 or an integer number of samples"
-            in str(excinfo.value)
-        )
-
-    @pytest.mark.it("Raises TypeError if random seed is not a valid type")
-    def test_random_seed_invalid_type(self, linear_processed_df):
-        with pytest.raises(TypeError) as excinfo:
-            linear_train_test_datasets(
-                linear_processed_df, target_col="price", random_seed="42"
-            )
-        assert "random_seed must be an integer" in str(excinfo.value)
-
-    @pytest.mark.it(
-        "Raises ValueError if random seed is outside the valid integer range"
-    )
-    def test_random_seed_outside_integer_limits(self, linear_processed_df):
-        with pytest.raises(ValueError) as excinfo:
-            linear_train_test_datasets(
-                linear_processed_df, target_col="price", random_seed=-1
-            )
-        assert "random_seed must be an integer in the range [0, 2**32 - 1]" in str(
-            excinfo.value
         )
 
 
@@ -202,7 +163,7 @@ class TestEmbeddingsPreprocessing:
         )
         assert isinstance(X_num, pd.DataFrame)
         assert isinstance(X_cat, pd.DataFrame)
-        assert isinstance(y, pd.DataFrame)
+        assert isinstance(y, pd.Series)
         assert isinstance(mappings, dict)
 
     @pytest.mark.it("Categorical columns are encoded")
@@ -255,7 +216,7 @@ class TestSplitTensorise:
         split_and_tensorise(X_num, X_cat, y)
         pd.testing.assert_frame_equal(copy_X_num, X_num)
         pd.testing.assert_frame_equal(copy_X_cat, X_cat)
-        pd.testing.assert_frame_equal(copy_y, y)
+        pd.testing.assert_series_equal(copy_y, y)
 
     @pytest.mark.it("Returns tensors")
     def test_returns_tensors(self, embeddings_preprocessing_data):
@@ -272,28 +233,53 @@ class TestSplitTensorise:
         assert X_num_train.shape[0] == X_cat_train.shape[0] == y_train.shape[0]
         assert X_num_test.shape[0] == X_cat_test.shape[0] == y_test.shape[0]
 
+    @pytest.mark.it("Train and test sizes returned as expected")
+    def test_traintest_sizes(self):
+        test_X_num = pd.DataFrame(np.random.rand(100, 3))
+        test_X_cat = pd.DataFrame(np.random.rand(100, 2)).astype(dtype="int32")
+        test_y = pd.Series(np.random.rand(100))
+        X_num_train, X_num_test, X_cat_train, X_cat_test, y_train, y_test = (
+            split_and_tensorise(test_X_num, test_X_cat, test_y)
+        )
+        assert len(X_num_train) == len(X_cat_train) == len(y_train) == 80
+        assert len(X_num_test) == len(X_cat_test) == len(y_test) == 20
 
-# @pytest.mark.describe("Tensor converter exception handling")
-# class TestTensorConverterExceptions:
 
-#     @pytest.mark.it("Raises TypeError if either input is not a dataframe")
-#     def test_typeerror_input_not_dataframe(self):
-#         with pytest.raises(TypeError) as excinfo:
-#             tensor_converter("not a dataframe", "not a dataframe")
-#         assert "Inputs must both be a pandas dataframe" in str(excinfo.value)
+@pytest.mark.describe("Split and tensorise exception handling")
+class TestSplitTensoriseExceptions:
 
-#     @pytest.mark.it("Raises TypeError if either input contains non-numeric data")
-#     def test_typeerror_non_numeric_cols(self, cleansed_df):
-#         X = cleansed_df.drop(columns="price", axis=1)
-#         y = cleansed_df[["price"]]
-#         with pytest.raises(TypeError) as excinfo:
-#             tensor_converter(X, y)
-#         assert "Inputs must not contain non-numeric values" in str(excinfo.value)
+    @pytest.mark.it("Raises TypeError if any input data is not a dataframe")
+    def test_typeerror_any_input_data_not_dataframe(
+        self, embeddings_preprocessing_data
+    ):
+        _, _, y, _ = embeddings_preprocessing_data
+        with pytest.raises(TypeError) as excinfo:
+            split_and_tensorise("not a dataframe", "not a dataframe", y)
+        assert "Input features must be pandas dataframe" in str(excinfo.value)
 
-#     @pytest.mark.it("Raises ValueError if inputs differ in length")
-#     def test_valueerror_differing_length_inputs(self, linear_processed_training_data):
-#         X, _, y, _ = linear_processed_training_data
-#         y = y.head()
-#         with pytest.raises(ValueError) as excinfo:
-#             tensor_converter(X, y)
-#         assert "X and y lengths must match" in str(excinfo.value)
+    @pytest.mark.it("Raises TypeError if target is not a series")
+    def test_target_not_a_series(self, embeddings_preprocessing_data):
+        X_num, X_cat, _, _ = embeddings_preprocessing_data
+        with pytest.raises(TypeError) as excinfo:
+            split_and_tensorise(X_num, X_cat, "not a series")
+        assert "Target must be a pandas series" in str(excinfo.value)
+
+    @pytest.mark.it("Raises ValueError for non-numeric values in numerical features")
+    def test_valueerror_for_non_numeric_in_x_num(
+        self, cleansed_df, embeddings_preprocessing_data
+    ):
+        _, X_cat, y, _ = embeddings_preprocessing_data
+        X_num = cleansed_df.drop(columns="price")
+        with pytest.raises(ValueError) as excinfo:
+            split_and_tensorise(X_num, X_cat, y)
+        assert "X_num must only contain numeric values" in str(excinfo.value)
+
+    @pytest.mark.it("Raises ValueError for non-integer values in categorical features")
+    def test_valueerror_for_non_integer_in_x_cat(
+        self, cleansed_df, embeddings_preprocessing_data
+    ):
+        X_num, _, y, _ = embeddings_preprocessing_data
+        X_cat = cleansed_df.drop(columns="price")
+        with pytest.raises(ValueError) as excinfo:
+            split_and_tensorise(X_num, X_cat, y)
+        assert "X_cat must only contain integer category code" in str(excinfo.value)
